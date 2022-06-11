ad67d635b65de3dfca846459471e3a23e904ca96
Anna-Jin <anna.jin.6848@gmail.com>
2022-06-11 18:56:58 +0900

    #1 Spring security & JWT 토큰 설정 파일

---
+++ src/main/java/com/homework/homework_week2/config/security/user/CustomUserDetailService.java
@@ -0,0 +1,21 @@
+package com.homework.homework_week2.config.security.user;
+
+import com.homework.homework_week2.user.repoistory.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+    }
+}
