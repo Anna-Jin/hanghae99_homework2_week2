3a026c43410c30ea6c3cba2d02e5b8ebd0b6155b
Anna-Jin <anna.jin.6848@gmail.com>
2022-06-13 11:55:46 +0900

    #3 주석 위치 변경

--- src/main/java/com/homework/homework_week2/config/security/WebSecurityConfig.java
+++ src/main/java/com/homework/homework_week2/config/security/WebSecurityConfig.java
@@ -36,12 +36,15 @@ public class WebSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .httpBasic().disable() // rest api 만을 고려하여 기본 설정은 해제하겠습니다.
+                .cors()
+                .and()
                 .csrf().disable() // csrf 보안 토큰 disable처리.
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 토큰 기반 인증이므로 세션 역시 사용하지 않습니다.
                 .and()
                 .authorizeRequests() // 요청에 대한 사용권한 체크
-                .antMatchers("/user/**").hasRole("USER")
-                .anyRequest().permitAll() // 그외 나머지 요청은 누구나 접근 가능
+                .antMatchers("/user/**").hasRole("USER") // 사용권한 체크 url을 추가하고 싶다면 이 밑에 anyMatchers 이용
+                .anyRequest()
+                .permitAll() // 그외 나머지 요청은 누구나 접근 가능
                 .and()
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
                         UsernamePasswordAuthenticationFilter.class);
