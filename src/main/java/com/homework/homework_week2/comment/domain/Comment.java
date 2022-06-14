5b369c6abf57cbdf10f21e16b9f69e20a12f93c5
Anna-Jin <anna.jin.6848@gmail.com>
2022-06-14 18:13:22 +0900

    #17 comment entity 설계

--- src/main/java/com/homework/homework_week2/comment/domain/Comment.java
+++ src/main/java/com/homework/homework_week2/comment/domain/Comment.java
@@ -1,4 +1,44 @@
 package com.homework.homework_week2.comment.domain;
 
-public class Comment {
+import com.homework.homework_week2.post.domain.Post;
+import com.homework.homework_week2.timestamp.Timestamped;
+import com.homework.homework_week2.user.domain.User;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Comment extends Timestamped {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false, columnDefinition = "text")
+    private String content;
+
+    // 연관관계 매핑
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "POST_ID", nullable = false)
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "USER_ID", nullable = false)
+    private User user;
+
+    @Builder
+    public Comment (String content, Post post, User user) {
+        this.content = content;
+        this.post = post;
+        this.user = user;
+    }
+
+    public void update (String content) {
+        this.content = content;
+    }
+
 }
