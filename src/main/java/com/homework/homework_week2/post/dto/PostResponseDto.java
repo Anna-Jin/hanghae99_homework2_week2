ef3e0c6f366431ae2a6bdd6b2ebf6c4fcda04489
Anna-Jin <anna.jin.6848@gmail.com>
2022-06-15 01:32:36 +0900

    #3 코드 리팩토링
    postDto를 postResponseDto로 명칭 변경

--- src/main/java/com/homework/homework_week2/post/dto/PostResponseDto.java
+++ src/main/java/com/homework/homework_week2/post/dto/PostResponseDto.java
@@ -1,6 +1,5 @@
 package com.homework.homework_week2.post.dto;
 
-import com.homework.homework_week2.comment.domain.Comment;
 import com.homework.homework_week2.comment.dto.CommentResponseDto;
 import lombok.Builder;
 import lombok.Getter;
@@ -8,23 +7,23 @@ import lombok.Getter;
 import java.util.List;
 
 @Getter
-public class PostDto {
+public class PostResponseDto {
     private Long postId;
     private String title;
     private String content;
     private String imageUrl;
-    private int viewCount = 0;
+    private Long viewCount;
 
     private List<CommentResponseDto> comments;
     private String createdAt;
 
     @Builder
-    public PostDto(Long postId, String title, String content, String imageUrl, int viewCount, List<CommentResponseDto> comments, String createdAt) {
+    public PostResponseDto(Long postId, String title, String content, String imageUrl, Long viewCount, List<CommentResponseDto> comments, String createdAt) {
         this.postId = postId;
         this.title = title;
         this.content = content;
         this.imageUrl = imageUrl;
-        this.viewCount = viewCount + 1;
+        this.viewCount = viewCount;
         this.comments = comments;
         this.createdAt = createdAt;
     }
