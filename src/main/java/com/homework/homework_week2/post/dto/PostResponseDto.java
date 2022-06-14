122559c8d6ed69bab8ff94af1d01783810f09b49
Anna-Jin <anna.jin.6848@gmail.com>
2022-06-15 03:56:19 +0900

    #3 코드 리팩토링

--- src/main/java/com/homework/homework_week2/post/dto/PostResponseDto.java
+++ src/main/java/com/homework/homework_week2/post/dto/PostResponseDto.java
@@ -1,30 +1,23 @@
 package com.homework.homework_week2.post.dto;
 
 import com.homework.homework_week2.comment.dto.CommentResponseDto;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 import java.util.List;
 
 @Getter
+@Builder @AllArgsConstructor
 public class PostResponseDto {
     private Long postId;
     private String title;
     private String content;
     private String imageUrl;
     private Long viewCount;
-
     private List<CommentResponseDto> comments;
     private String createdAt;
+    private boolean isLike;
+    private Long likeCount;
 
-    @Builder
-    public PostResponseDto(Long postId, String title, String content, String imageUrl, Long viewCount, List<CommentResponseDto> comments, String createdAt) {
-        this.postId = postId;
-        this.title = title;
-        this.content = content;
-        this.imageUrl = imageUrl;
-        this.viewCount = viewCount;
-        this.comments = comments;
-        this.createdAt = createdAt;
-    }
 }
