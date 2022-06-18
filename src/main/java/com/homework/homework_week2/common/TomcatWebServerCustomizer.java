91838eeb71094a57034d8f91458327be556bcf27
Anna-Jin <anna.jin.6848@gmail.com>
2022-06-18 22:25:17 +0900

    #39 The valid characters are defined in RFC 7230 and RFC 3986 에러를 잡기 위한 코드

---
+++ src/main/java/com/homework/homework_week2/common/TomcatWebServerCustomizer.java
@@ -0,0 +1,20 @@
+package com.homework.homework_week2.common;
+
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TomcatWebServerCustomizer
+        implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
+
+    /**
+     * 톰캣에 옵션 추가
+     *
+     * @param factory
+     */
+    @Override
+    public void customize(TomcatServletWebServerFactory factory) {
+        factory.addConnectorCustomizers(connector -> connector.setProperty("relaxedQueryChars", "<>[\\]^`{|}"));
+    }
+}
