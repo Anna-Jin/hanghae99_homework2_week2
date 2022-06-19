1b139ac3be3e37f192f45df931abdf050427d823
Anna-Jin <anna.jin.6848@gmail.com>
2022-06-19 12:33:04 +0900

    CORS에 delete 메소드 추가

--- src/main/java/com/homework/homework_week2/config/security/WebSecurityConfig.java
+++ src/main/java/com/homework/homework_week2/config/security/WebSecurityConfig.java
@@ -65,7 +65,7 @@ public class WebSecurityConfig {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOriginPatterns(Arrays.asList("*"));
-        configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT"));
+        configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
         configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
